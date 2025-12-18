8c63676b1e2bb777ff57a83b1f24bf815a0fbac4
maxiscritikal <101420657+maxiscritikal@users.noreply.github.com>
2025-12-18 16:05:00 +0000

    Final commit ready to grade!

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -1,3 +1,5 @@
+//Max Korsa, 12/18/25. PD: 4
+//This program uses stacks and queues to manipulate user input.
 import java.util.Scanner;
 import java.util.Stack;
 import java.util.Queue;
@@ -10,27 +12,75 @@ class StackQueueMini {
 
     Stack<Integer> myStack = new Stack<>();
 
+    System.out.println("Enter a number 5 times: ");
     for (int x = 1; x < 6; x++)
     {
-      System.out.println("Enter your " + x + " number: ");
       int temp = in.nextInt();
       myStack.push(temp);
-    }
+    }//end for loop
   
-    Stack<Integer> doubleStack = new Stack<>();
-    while (!myStack.isEmpty())
+    Stack<Integer> tempStack = new Stack<>();
+
+    while (!myStack.isEmpty()) 
     {
-      int temp = myStack.pop();
-      doubleStack.push(temp);
-      doubleStack.push(temp);
-    }
+      tempStack.push(myStack.pop());
+    }//end while loop
+    
+    while (!tempStack.isEmpty()) 
+    {
+      int temp = tempStack.pop();
+      myStack.push(temp);
+      myStack.push(temp);
+    }//end while loop
 
     System.out.println("Here is your new Stack after the method:");
-    while (!doubleStack.isEmpty())
+    while (!myStack.isEmpty()) 
     {
-      System.out.print(doubleStack.pop() + ", ");
-    }
+      System.out.print(myStack.pop() + ", ");
+    }//end while loop
 
+    Queue<Integer> myQueue = new LinkedList<>();
 
+    System.out.println();
+    System.out.println("Enter a number 10 times: ");
+    for (int x = 1; x < 11; x++)
+    {
+      int temp = in.nextInt();
+      myQueue.add(temp);
+    }//end for loop
+
+    Queue<Integer> evenQueue = new LinkedList<>();
+    Queue<Integer> oddQueue = new LinkedList<>();
+    Queue<Integer> finalQueue = new LinkedList<>();
+
+    while (!myQueue.isEmpty())
+    {
+      if (myQueue.peek() % 2 == 0)
+        {
+          evenQueue.add(myQueue.remove());
+        }//end if statement
+      else
+        {
+          oddQueue.add(myQueue.remove());
+        }//end else statement
+    }//end while loop
+
+    while (!evenQueue.isEmpty())
+    {
+      finalQueue.add(evenQueue.remove());
+    }//end while loop
+
+    while (!oddQueue.isEmpty())
+    {
+      finalQueue.add(oddQueue.remove());
+    }//end while loop
+
+    System.out.println("Here is your new Queue after the method:");
+    while (!finalQueue.isEmpty())
+    {
+      System.out.print(finalQueue.remove() + ", ");
+    }//end while loop
+
+    in.close();
   } // end main
 } // end class
